0dd0d86fd798a55960be5cbe7d5cfc41852ccbd5
Manthan Agrawal <agrawalmanthan0@gmail.com>
2020-04-05 15:46:48 +0530

    intial code

---
+++ src/main/java/com/learningSpring/testingSpring/TestingSpringApplication.java
@@ -0,0 +1,15 @@
+package com.learningSpring.testingSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TestingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingSpringApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/learningSpring/testingSpring/TestingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningSpring.testingSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
